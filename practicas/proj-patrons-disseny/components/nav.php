c38fe582e66f3dc50c04d628ec6f386732f6a215
Gabrielbe17 <70650366+Gabrielbe17@users.noreply.github.com>
2025-02-10 15:07:05 +0000

    component nav, ruta cambiada

--- practicas/proj-patrons-disseny/components/nav.php
+++ practicas/proj-patrons-disseny/components/nav.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-sm navbar-dark bg-dark text-light">
     <div class="container-fluid d-flex justify-content-between p-2">
-        <a class="navbar-brand" href="/index.php">
+        <a class="navbar-brand" href="/UF2/practicas/proj-patrons-disseny/index.php">
             Home
             <i class="fa-solid fa-house"></i>
         </a>
@@ -15,28 +15,28 @@
                     <div class="position-relative">
                         <a class="nav-link active" role="button" aria-current="page" data-bs-toggle="dropdown" id="dropdown2" aria-haspopup="true" aria-expanded="false">Patrones Creacionales</a>
                         <div class="dropdown-menu" aria-labelledby="dropdown2">
-                            <a class="dropdown-item" href="/pages/creacionals.php">Introducción</a>
-                            <a class="dropdown-item" href="/patrons/creacionals/abstract-factory.php">Abstract</a>
-                            <a class="dropdown-item" href="/patrons/creacionals/factory.php">Factory</a>
-                            <a class="dropdown-item" href="/patrons/creacionals/singleton.php">Singleton</a>
+                            <a class="dropdown-item" href="/UF2/practicas/proj-patrons-disseny/pages/creacionals.php">Introducción</a>
+                            <a class="dropdown-item" href="/UF2/practicas/proj-patrons-disseny/patrons/creacionals/abstract-factory.php">Abstract</a>
+                            <a class="dropdown-item" href="/UF2/practicas/proj-patrons-disseny/patrons/creacionals/factory.php">Factory</a>
+                            <a class="dropdown-item" href="/UF2/practicas/proj-patrons-disseny/patrons/creacionals/singleton.php">Singleton</a>
                         </div>
                     </div>
                     <div class="position-relative">
                         <a class="nav-link active" role="button" aria-current="page" data-bs-toggle="dropdown" id="dropdown1" aria-haspopup="true" aria-expanded="false">Patrones Estructurales</a>
                         <div class="dropdown-menu" aria-labelledby="dropdown1">
-                            <a class="dropdown-item" href="/pages/estructurals.php">Introducción</a>
-                            <a class="dropdown-item" href="/patrons/estructurals/adapter.php">Adapter</a>
-                            <a class="dropdown-item" href="/patrons/estructurals/bridge.php">Bridge</a>
-                            <a class="dropdown-item" href="/patrons/estructurals/composite.php">Composite</a>
+                            <a class="dropdown-item" href="/UF2/practicas/proj-patrons-disseny/pages/estructurals.php">Introducción</a>
+                            <a class="dropdown-item" href="/UF2/practicas/proj-patrons-disseny/patrons/estructurals/adapter.php">Adapter</a>
+                            <a class="dropdown-item" href="/UF2/practicas/proj-patrons-disseny/patrons/estructurals/bridge.php">Bridge</a>
+                            <a class="dropdown-item" href="/UF2/practicas/proj-patrons-disseny/patrons/estructurals/composite.php">Composite</a>
                         </div>
                     </div>
                     <div class="position-relative">
                         <a class="nav-link active" role="button" aria-current="page" data-bs-toggle="dropdown" id="dropdown3" aria-haspopup="true" aria-expanded="false">Patrones de Comportamiento</a>
                         <div class="dropdown-menu" aria-labelledby="dropdown3">
-                            <a class="dropdown-item" href="/pages/comportament.php">Introducción</a>
-                            <a class="dropdown-item" href="/patrons/comportament/chain_of_responsibility.php">Chain of Responsibility</a>
-                            <a class="dropdown-item" href="/patrons/comportament/command.php">Command</a>
-                            <a class="dropdown-item" href="/patrons/comportament/iterator.php">Iterator</a>
+                            <a class="dropdown-item" href="/UF2/practicas/proj-patrons-disseny/pages/comportament.php">Introducción</a>
+                            <a class="dropdown-item" href="/UF2/practicas/proj-patrons-disseny/patrons/comportament/chain_of_responsibility.php">Chain of Responsibility</a>
+                            <a class="dropdown-item" href="/UF2/practicas/proj-patrons-disseny/patrons/comportament/command.php">Command</a>
+                            <a class="dropdown-item" href="/UF2/practicas/proj-patrons-disseny/patrons/comportament/iterator.php">Iterator</a>
                         </div>
                     </div>
                 </div>
