c8431063487458b6510054306314ca37d626961d
Gabrielbe17 <70650366+Gabrielbe17@users.noreply.github.com>
2025-02-20 17:39:43 +0000

    Ultimas 3 noticias y noticias pagina blog

--- practicas/agen-bootstrap-main/theme/blog.php
+++ practicas/agen-bootstrap-main/theme/blog.php
@@ -1,3 +1,12 @@
+<?php
+  require_once './config/config.php';
+
+
+  $newsResult = $mysqli->query("SELECT * FROM NEWS ORDER BY newDate DESC");
+
+  $news = $newsResult->fetch_all(MYSQLI_ASSOC);
+
+?>
 <!DOCTYPE html>
 
 <!--
@@ -102,7 +111,19 @@
 <section class="section">
   <div class="container">
     <div class="row">
-      <div class="col-lg-4 col-md-6 mb-4">
+      <?php foreach ($news as $new) {
+          echo "<div class='col-lg-4 col-md-6 mb-4 mb-lg-0'>
+              <article class='card'>
+                <img src='images/blog/post-1.jpg' alt='post-thumb' class='card-img-top mb-2'>
+                <div class='card-body p-0'>
+                  <time>{$new['newDate']}</time>
+                  <a href='blog-single' class='h4 card-title d-block my-3 text-dark hover-text-underline'>{$new['title']}</a>
+                  <a href='#' class='btn btn-transparent'>Read more</a>
+                </div>
+              </article>
+            </div>";
+        } ?>
+      <!-- <div class="col-lg-4 col-md-6 mb-4">
         <article class="card">
           <img src="images/blog/post-1.jpg" alt="post-thumb" class="card-img-top mb-2">
           <div class="card-body p-0">
@@ -200,7 +221,7 @@
             <a href="blog-single.php" class="btn btn-transparent">Read more</a>
           </div>
         </article>
-      </div>
+      </div> -->
     </div>
   </div>
 </section>
--- practicas/agen-bootstrap-main/theme/index.php
+++ practicas/agen-bootstrap-main/theme/index.php
@@ -5,12 +5,13 @@
   // query 
   echo "<br>";
   $result = $mysqli->query("SELECT * FROM PROJECTS ORDER BY id DESC");
+  $resultNews = $mysqli->query("SELECT * FROM NEWS ORDER BY newDate DESC LIMIT 3");
   // print_r($result);
 
 
   // convertir obeto result a array asociativo
   $projects = $result->fetch_all(MYSQLI_ASSOC);
-
+  $latestNews = $resultNews->fetch_all(MYSQLI_ASSOC);
   
 ?>
 
@@ -487,7 +488,7 @@
       </div>
     </div>
     <div class="row">
-      <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
+      <!-- <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
         <article class="card">
           <img src="images/blog/post-1.jpg" alt="post-thumb" class="card-img-top mb-2">
           <div class="card-body p-0">
@@ -497,8 +498,21 @@
             <a href="#" class="btn btn-transparent">Read more</a>
           </div>
         </article>
-      </div>
-      <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
+      </div> -->
+      <?php foreach ($latestNews as $new) {
+        echo "<div class='col-lg-4 col-md-6 mb-4 mb-lg-0'>
+            <article class='card'>
+              <img src='images/blog/post-1.jpg' alt='post-thumb' class='card-img-top mb-2'>
+              <div class='card-body p-0'>
+                <time>{$new['newDate']}</time>
+                <a href='blog-single' class='h4 card-title d-block my-3 text-dark hover-text-underline'>{$new['title']}</a>
+                <a href='#' class='btn btn-transparent'>Read more</a>
+              </div>
+            </article>
+          </div>";
+      } ?>
+
+      <!-- <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
         <article class="card">
           <img src="images/blog/post-2.jpg" alt="post-thumb" class="card-img-top mb-2">
           <div class="card-body p-0">
@@ -519,7 +533,7 @@
             <a href="#" class="btn btn-transparent">Read more</a>
           </div>
         </article>
-      </div>
+      </div> -->
     </div>
   </div>
 </section>
