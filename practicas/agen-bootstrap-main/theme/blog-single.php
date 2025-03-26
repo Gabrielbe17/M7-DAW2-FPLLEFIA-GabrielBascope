4851c19cadb9f74351fdb9c851b91702dd859902
Gabrielbe17 <70650366+Gabrielbe17@users.noreply.github.com>
2025-03-26 15:32:06 +0000

    cambios rutas y  header

--- practicas/agen-bootstrap-main/theme/blog-single.php
+++ practicas/agen-bootstrap-main/theme/blog-single.php
@@ -97,51 +97,65 @@
 
 <body>
   
-
 <header class="navigation fixed-top">
-  <nav class="navbar navbar-expand-lg navbar-dark">
-    <a class="navbar-brand" href="index.php"><img src="images/logo.png" alt="Egen"></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation"
-      aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+    <nav class="navbar navbar-expand-lg navbar-dark">
+      <a class="navbar-brand" href="index.php"><img src="images/logo.png" alt="Egen"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation"
+        aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-    <div class="collapse navbar-collapse text-center" id="navigation">
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="about.php">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="services.php">Services</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="blog.php">Blog</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="portfolio.php">Portfolio</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pages</a>
-          <div class="dropdown-menu">
-            <a class="dropdown-item" href="team.php">Team</a>
-            <a class="dropdown-item" href="team-single.php">Team Details</a>
-            <a class="dropdown-item" href="career.php">Career</a>
-            <a class="dropdown-item" href="career-single.php">Career Details</a>
-            <a class="dropdown-item" href="blog-single.php">Blog Details</a>
-            <a class="dropdown-item" href="pricing.php">Pricing</a></a>
-            <a class="dropdown-item" href="faqs.php">FAQ's</a>
-          </div>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="contact.php">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
-</header>
+
+      <div class="collapse navbar-collapse text-center" id="navigation">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.php">About</a>
+          </li>
+          <!-- <li class="nav-item">
+            <a class="nav-link" href="services.php">Services</a>
+          </li> -->
+          <li class="nav-item">
+            <a class="nav-link" href="blog.php">Blog</a>
+          </li>
+          <!-- <li class="nav-item">
+            <a class="nav-link" href="portfolio.php">Portfolio</a>
+          </li> -->
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pages</a>
+            <div class="dropdown-menu">
+              <!-- <a class="dropdown-item" href="team.php">Team</a> -->
+              <!-- <a class="dropdown-item" href="team-single.php">Team Details</a> -->
+              <!-- <a class="dropdown-item" href="career.php">Career</a> -->
+              <!-- <a class="dropdown-item" href="career-single.php">Career Details</a> -->
+              <!-- <a class="dropdown-item" href="blog-single.php">Blog Details</a> -->
+              <a class="dropdown-item" href="pricing.php">Pricing</a></a>
+              <a class="dropdown-item" href="./faqs.php">FAQ's</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.php">Contact</a>
+          </li>
+        </ul>
+
+
+        <?php if (isset($_SESSION['user_picture'])) : ?>
+          <img src="<?= $_SESSION['user_picture'] ?>" class="rounded-circle img-fluid" style="width: 50px; height: 50px; object-fit: cover;" alt="User Avatar">
+          <p class="mt-3"><?= $_SESSION['user_name'] ?></p>
+          <a href="logout.php" class="btn btn-primary btn-sm">Cerrar Sesión</a>
+
+          <?php if ($_SESSION['user_role'] === 'admin') : ?>
+            <a href="admin.php" class="btn btn-light btn-sm">
+              <img src="./assets/admin.png" class="rounded-circle img-fluid" style="width: 30px; height: 30px; object-fit: cover;" alt="Admin Icon">
+            </a>
+          <?php endif; ?>
+
+        <?php endif; ?>
+      </div>
+    </nav>
+  </header>
 
 <!-- page-title -->
 <section class="page-title bg-cover" data-background="images/backgrounds/page-title.jpg">
